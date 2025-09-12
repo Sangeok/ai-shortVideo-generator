21353ab270035a1be2f67270ca086250958d1b24
HamSangEok <hamsoo159@naver.com>
2025-09-13 00:21:51 +0900

    refactor : delete google image gen model

--- app/configs/AiModel.ts
+++ app/configs/AiModel.ts
@@ -5,13 +5,6 @@ import OpenAI from "openai";
 const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
-const imageGenerationConfig = {
-  temperature: 1,
-  topP: 0.95,
-  topK: 40,
-  responseModalities: ["Text", "Image"],
-};
-
 const generationConfig = {
   temperature: 1,
   topP: 0.95,
@@ -24,20 +17,9 @@ const model = genAI.getGenerativeModel({
   model: "gemini-2.5-flash",
 });
 
-const imageGeneratorModel_ID = "gemini-2.0-flash-exp";
-
-export const imageGeneratorModel = genAI.getGenerativeModel({
-  model: imageGeneratorModel_ID,
-  generationConfig: imageGenerationConfig,
-});
-
 export const Openai = new OpenAI({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
-
-// const imageGeneratorModel = genAI.getGenerativeModel({
-//   model: "gemini-2.0-flash-exp",
-// });
 
 export const generateScript = model.startChat({
   generationConfig,
@@ -48,9 +30,3 @@ export const generateImageScript = model.startChat({
   generationConfig,
   history: [],
 });
-
-// export const generateImage = imageGeneratorModel.startChat({
-//   generationConfig: imageGenerationConfig,
-//   history: [],
-// });
-// run();
